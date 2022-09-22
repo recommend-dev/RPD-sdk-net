4e4ab3fffe105abaa9c92471443b7ac3f454c636
Matko Kostelic <matko@recommend.co>
2022-09-22 10:22:11 +0200

    example update

--- Recommend.SDK.Test/Program.cs
+++ Recommend.SDK.Test/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Recommend.SDK;
 
-Recommend.SDK.APIClient client = new APIClient("0fe42cff-9151-4125-a2ed-133c6f3e55eb", "https://rpd-api-stage.azurewebsites.net/apikeys", true);
+Recommend.SDK.APIClient client = new APIClient("your-api-key-here");
 
 Console.WriteLine("Connection test result: " + await client.TestConnection());
 
-var referralCheck = await client.ReferralCheck("2fb48e28-c83c-4400-9c67-e453c28028a5");
+var referralCheck = await client.ReferralCheck("valid-rcmdn-code");
 Console.WriteLine("Referral check: " + System.Text.Json.JsonSerializer.Serialize(referralCheck) );
 
-var badReferralCheck = await client.ReferralCheck("2fb48e28-0000-4400-9c67-e453c28028a5");
+var badReferralCheck = await client.ReferralCheck("invalid-rcmdn-code");
 Console.WriteLine("Referral check: " + System.Text.Json.JsonSerializer.Serialize(badReferralCheck) );
