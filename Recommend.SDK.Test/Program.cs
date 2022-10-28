9dd0388e975a0d7b534ada86eafeef311f89f76d
Matko <matko@recommend.co>
2022-10-28 10:23:45 +0200

    updated test project references and test steps

--- Recommend.SDK.Test/Program.cs
+++ Recommend.SDK.Test/Program.cs
@@ -1,11 +1,15 @@
 ﻿using Recommend.SDK;
 
+// initialize SDK with your API key and optional API URL
 Recommend.SDK.APIClient client = new APIClient("your-api-key-here");
 
+// this call is used to test connection, in PROD can be removed. Returns true if connection parameters are ok, false if there is an issue with code.
 Console.WriteLine("Connection test result: " + await client.TestConnection());
 
+ // generate conversion when purchase occurs - send rcmndref code. Returns response with status code 200 if connection and referral code are ok
 var referralCheck = await client.ReferralCheck("valid-rcmdn-code");
 Console.WriteLine("Referral check: " + System.Text.Json.JsonSerializer.Serialize(referralCheck) );
 
+// invalid code or wrong API endpoint URL will return null response
 var badReferralCheck = await client.ReferralCheck("invalid-rcmdn-code");
 Console.WriteLine("Referral check: " + System.Text.Json.JsonSerializer.Serialize(badReferralCheck) );
